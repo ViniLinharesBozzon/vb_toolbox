d86a9be0d4822688aa771101b985bdce4e3e20e7
ViniLinharesBozzon <vinicius.bozzon@gmail.com>
2026-04-29 04:27:33 -0300

    Alterando pro nome padrão databricks

--- src/vb_toolbox/ingestion/spark_io.py
+++ src/vb_toolbox/ingestion/spark_io.py
@@ -1,7 +1,7 @@
 # Preciso algo que pegue seu spark ou pandas dataframe e transforme em arquivo parquet isinstance(v_testeTexto_pd,pd.DataFrame)
 # E preciso que faça a camada de delta parquet
 
-def f_set_createDatabase(
+def f_set_createSchema(
         p_schema_name: str
 ) -> None:
     """
@@ -37,3 +37,42 @@ def f_set_dataframe2Parquet(
     else:
         raise ValueError("❌ Exportação sem ser via arquivo único ainda não desenvolvido. Favor alterar o parametro: p_uniqueFile")
     return None
+
+
+def f_save_dataframe_to_bronze(
+     p_dataframe                            # O objeto DataFrame que você já criou
+   , p_table_name   : str                   # Nome da tabela (ex: alura_alunos)
+   , p_schema_name  : str                   # Nome do Banco de Dados
+   , p_catalog_name : str = "bronze"
+   , p_mode         : str = "append"        # 'append' (adicionar) ou 'overwrite' (sobrescrever)
+):
+    """
+    Grava um DataFrame da memória para uma Tabela Delta Física (Batch/Job).
+    Ideal para cargas manuais ou agendadas (não-streaming).
+    """
+    
+    # Criando o database caso não exista
+    f_set_createSchema(p_schema_name)
+    
+    # Monta o nome final: bronze.alura_alunos
+    full_table_name = f"{p_catalog_name}.{p_schema_name}.{p_table_name}"
+    
+    print(f"--- Iniciando Gravação (Batch) ---")
+    print(f"Tabela Alvo: {full_table_name}")
+    print(f"Modo: {p_mode.upper()}")
+    
+    # 2. Configuração da Escrita
+    writer = p_dataframe.write.format("delta").mode(p_mode)
+    
+    # 3. Tratamento de Schema (A mágica da Bronze)
+    if p_mode == "append":
+        # Se for adicionar, aceita colunas novas que aparecerem (Evolução de Schema)
+        writer = writer.option("mergeSchema", "true")
+    elif p_mode == "overwrite":
+        # Se for sobrescrever, força o novo schema do DataFrame sobre a tabela antiga
+        writer = writer.option("overwriteSchema", "true")
+        
+    # 4. Ação Física (Grava no disco e registra no Metastore)
+    writer.saveAsTable(full_table_name)
+    
+    print(f"--- Sucesso! Dados gravados em {full_table_name} ---")
