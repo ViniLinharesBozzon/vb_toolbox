4582cdacecfab78212305d62fdea43e3fa155298
ViniLinharesBozzon <vinicius.bozzon@gmail.com>
2026-04-29 04:09:19 -0300

    spark: função para salvar dataframe em parquet

---
+++ src/vb_toolbox/ingestion/spark_io.py
@@ -0,0 +1,21 @@
+# Preciso algo que pegue seu spark ou pandas dataframe e transforme em arquivo parquet isinstance(v_testeTexto_pd,pd.DataFrame)
+# E preciso que faça a camada de delta parquet
+def f_set_dataframe2Parquet(
+    p_dataframe
+ ,  p_pathTarget    : str
+ ,  p_uniqueFile    : bool = True
+ ) -> None:
+    """
+    Args:
+    p_dataframe: Dataframe Spark que será exportado
+    p_pathTarget (str): Caminho do arquivo que será exportado.        
+    p_uniqueFile (bool): Usado para export de arquivo único.
+
+    Returns:
+        None.
+    """
+    if p_uniqueFile: # Salva em arquivo único seu dataframe
+        p_dataframe.coalesce(1).write.mode("overwrite").parquet(p_pathTarget)
+    else:
+        raise ValueError("❌ Exportação sem ser via arquivo único ainda não desenvolvido. Favor alterar o parametro: p_uniqueFile")
+    return None
